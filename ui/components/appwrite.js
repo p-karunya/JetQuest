f923610377b290e03ed7eed0a1bd27ad121c5646
Karunya Penumalla <karunya.penumalla@gmail.com>
2025-02-15 18:56:26 -0800

    consolidated code

--- ui/components/appwrite.js
+++
@@ -1,7 +0,0 @@
-import { Client } from "appwrite";
-
-const appwrite = new Client()
-      .setProject('67b0a193001cdabb42e4')
-      .setEndpoint('https://cloud.appwrite.io/v1'); // Your API Endpoint
-
-export default appwrite;
